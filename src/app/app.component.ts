3bb5b2b5707dfd9fb26dad5f2653dd68b7fb6ce7
Reza Fathollahi <reza.fathollahi96@gmail.com>
2022-02-26 12:33:02 +0330

    set alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+ 
   title = 'weatherApp';
+
+  ngOnInit(): void {
+    alert(
+      ' برای گرفتن دیتا باید مشکل CORS policy برطرف شود. میتوانید اکستنشن  Moesif CORS را نصب و فعال کنید. '
+    )
+  }
+  
 }
